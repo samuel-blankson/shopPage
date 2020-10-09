50a764fb7057e8ec73ae8cec5bcec01f11ae09d4
samuel-blankson <samuel.blankson@amalitech.org>
2020-10-10 00:21:43 +0100

    updated with a user form

--- js/main.js
+++ js/main.js
@@ -1,23 +1,26 @@
+var cart = 0;
+
+function addItem(id,name,description,price,moreInfo){
+    let html = '';
+    html += '<div class="item"  data-id = "'+id+'">';
+    html += '<div class="name">'+name+'</div>';
+html +='<img src="assets/pic.jpg"></img>';
+html += '<div class="description">'+description+' </div>';
+html +=    '<div class="price">'+price+'</div>';
+html +=  ' <button  class="item-add">Add to cart</button>';
+html += '<button class="item-remove">Remove</button>';
+html +=   '<br/>';
+html +=  ' <a href="#" class="more-info-link">More info</a>';
+html +=   ' <div class="more-info">'+moreInfo+'</div>';
+html +=' </div>';
+
+$('#container').prepend(html);
+
+}
+
 $(document).ready(function(){
-    $('#button-create-item').on('click',function(){
-        let name = $('#input-create-item').val();
-        $('#input-create-item').val('');
 
-        let html = '';
-        html += '<div class="item">';
-        html += '<div class="name">'+name+'</div>';
-    html +='<img src="assets/pic.jpg"></img>';
-    html += '<div class="description">Lorem ipsum dolor sit amet consectetur  adipisicing elit. </div>';
-    html +=    '<div class="price">488</div>';
-    html +=  ' <button  class="item-add">Add to cart</button>';
-    html += '<button class="item-remove">Remove</button>';
-    html +=   '<br/>';
-    html +=  ' <a href="#" class="more-info-link">more info</a>';
-    html +=   ' <div class="more-info">Lorem ipsum dolor sit</div>';
-    html +=' </div>';
-
-
-    $('#container').prepend(html);
+ 
 
     $('#container').on('click','.item-remove',function(){
         $(this).parent().remove();
@@ -32,6 +35,56 @@ $(document).ready(function(){
         .animate({"opacitty":1.0, "margin-left":0},'fast');
     });
 
+    $.ajax('data/item.json',{
+        dataType : 'json',
+        contentType :'application/json',
+        cache : false
+    })
+      .done(function(response){
+          let items = response.items;
+          items.forEach(function(item){
+            addItem(item.id,item.name,item.description,item.price,item.moreInfo);
+          });
+          
+      })
+      .fail(function(request,errorType,errorMessage){
+        console.log(errorMessage);
+    })
+      .always(function(){
+
+      })
+
+
+      $('#container').on('click','.item-add',function(){
+        let id = $(this).parent().data('id');
+        $.ajax('data/addTocart.json',{
+          type:'post',
+          data : {id : id},
+          dataType : 'json',
+          contentType :'application/json',
+         
+        })
+        .done(function(response){
+         if(response.message === 'success'){
+           let price = response.price;
+           cart +=price;
+           
+           $('#cart-container').text('$'+cart);
+
+         }
+        });
+
+      })
+
+      $('#newsletter-checkbox').on('change',function(){
+        if($(this).is(':checked')){
+          $('#newsletter-frequency').fadeIn();
+        }else{
+          $('#newsletter-frequency').fadeOut();
+        }
+
+      })
+
+      $('#newsletter-checkbox').trigger('change');
 
     });
-});
