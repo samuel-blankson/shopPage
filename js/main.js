3024ba8f640bc143c150d4cc90af202c5965c3f9
samuel-blankson <samuel.blankson@amalitech.org>
2020-10-09 18:33:52 +0100

    ajax manipulations

--- js/main.js
+++ js/main.js
@@ -12,11 +12,26 @@ $(document).ready(function(){
     html +=  ' <button  class="item-add">Add to cart</button>';
     html += '<button class="item-remove">Remove</button>';
     html +=   '<br/>';
-    html +=  ' <a href="#">more info</a>';
+    html +=  ' <a href="#" class="more-info-link">more info</a>';
     html +=   ' <div class="more-info">Lorem ipsum dolor sit</div>';
     html +=' </div>';
 
 
     $('#container').prepend(html);
+
+    $('#container').on('click','.item-remove',function(){
+        $(this).parent().remove();
+    });
+
+    $('#container').on('click','.more-info-link',function(event){
+        event.preventDefault();
+
+        $(this).parent().find('.more-info').slideToggle('slow');
+        $(this)
+        .animate({"opacitty":0.5 ,"margin-left":10},'fast')
+        .animate({"opacitty":1.0, "margin-left":0},'fast');
+    });
+
+
     });
 });
